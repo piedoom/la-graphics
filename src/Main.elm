b3db53edeb63b17a2ae11d9985ebe290585b085f
Alexander Lozada <Alexander@alexander.local>
2018-10-17 16:52:33 -0500

    a

--- src/Main.elm
+++ src/Main.elm
@@ -1,35 +1,36 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), Size, buildUnsplashQuery, controls, design, getImage, init, main, px, update, view)
 
 import Browser
-import Html exposing (Html, text, div, h1, img, input)
-import Html.Attributes exposing (src, style, type_, value, step)
+import Html exposing (Html, div, h1, img, input, text)
+import Html.Attributes exposing (placeholder, src, step, style, type_, value)
 import Html.Events exposing (onInput)
-import Validate exposing (ifNotInt, validate)
 import Http exposing (Request)
+import Validate exposing (ifNotInt, validate)
+
+
 
 ---- MODEL ----
 
 
 type alias Model =
-    { controls : Controls
+    { width : Int
+    , height : Int
+    , query : String
+    , imageUrl : String
     }
 
-type alias Controls =
-    { width  : Int
-    , height : Int
-    , query  : String
-    , backgroundUrl : String
-    }
 
 init : ( Model, Cmd Msg )
 init =
-    ( { controls = 
-        { width = 1200
-        , height = 630
-        , query = ""
-        , backgroundUrl = ""
-        } 
-    }, Cmd.none )
+    ( { width = 1200
+      , height = 630
+      , query = ""
+      , imageUrl = ""
+      }
+    , Cmd.none
+    )
+
+
 
 ---- UPDATE ----
 
@@ -38,37 +39,59 @@ type Msg
     = NoOp
     | UpdateWidth String
     | UpdateHeight String
+    | UpdateQuery String
+    | UpdateImage (Result Http.Error String)
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp -> (model, Cmd.none)
+        NoOp ->
+            ( model, Cmd.none )
+
         UpdateWidth sizeString ->
             let
-                nestedData = model.controls
-                sizeMaybe = String.toInt sizeString
+                sizeMaybe =
+                    String.toInt sizeString
             in
-                case sizeMaybe of
-                    Just(size) -> 
-                        let 
-                            updatedData = { nestedData | width = size }
-                        in
-                            ({ model | controls = updatedData }, Cmd.none)
-                    Nothing ->
-                        (model, Cmd.none)
+            case sizeMaybe of
+                Just size ->
+                    ( { model | width = size }, Cmd.none )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
         UpdateHeight sizeString ->
             let
-                nestedData = model.controls
-                sizeMaybe = String.toInt sizeString
+                sizeMaybe =
+                    String.toInt sizeString
             in
-                case sizeMaybe of
-                    Just(size) -> 
-                        let 
-                            updatedData = { nestedData | height = size }
-                        in
-                            ({ model | controls = updatedData }, Cmd.none)
-                    Nothing ->
-                        (model, Cmd.none)
+            case sizeMaybe of
+                Just size ->
+                    ( { model | height = size }, Cmd.none )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+        UpdateQuery search ->
+            -- ( { model | query = search }, getImage model )
+            let
+                updatedModel =
+                    { model | query = search }
+
+                url =
+                    buildUnsplashQuery updatedModel
+            in
+            ( { model | imageUrl = url }, Cmd.none )
+
+        UpdateImage result ->
+            case result of
+                Ok newUrl ->
+                    ( { model | imageUrl = newUrl }, Cmd.none )
+
+                Err e ->
+                    ( model, Cmd.none )
+
 
 
 ---- VIEW ----
@@ -78,41 +101,64 @@ view : Model -> Html Msg
 view model =
     div []
         [ controls model
-        , design model ]
+        , design model
+        ]
+
 
 design : Model -> Html Msg
 design model =
-    let 
-        width = px model.controls.width
-        height = px model.controls.height
-        background = model.controls.backgroundUrl
+    let
+        width =
+            px model.width
+
+        height =
+            px model.height
+
+        background =
+            model.imageUrl
     in
-        div [ style "border" "1px rgba(0,0,0,0.3) solid"
-            , style "width" width
-            , style "height" height
-            , style "background-image" background ] 
-            [ text "asd" ]
+    div
+        [ style "border" "1px rgba(0,0,0,0.3) solid"
+        , style "width" width
+        , style "height" height
+        , style "background-image" ("url(" ++ background ++ ")")
+        ]
+        []
+
 
 controls : Model -> Html Msg
 controls model =
-    let 
-        width = String.fromInt model.controls.width
-        height = String.fromInt model.controls.height
+    let
+        width =
+            String.fromInt model.width
+
+        height =
+            String.fromInt model.height
     in
-        div []
-            [ input 
-                [ type_ "number"
-                , value width
-                , step "50"
-                , onInput UpdateWidth ] 
-                []
-            , input
-                [ type_ "number"
-                , value height
-                , step "50"
-                , onInput UpdateHeight ] 
-                []
+    div []
+        [ input
+            [ type_ "number"
+            , value width
+            , step "50"
+            , onInput UpdateWidth
             ]
+            []
+        , input
+            [ type_ "number"
+            , value height
+            , step "50"
+            , onInput UpdateHeight
+            ]
+            []
+        , input
+            [ type_ "text"
+            , placeholder "search"
+            , onInput UpdateQuery
+            ]
+            []
+        ]
+
+
 
 ---- PROGRAM ----
 
@@ -127,30 +173,48 @@ main =
         }
 
 
+
 --
+
 
 px : Int -> String
 px attr =
-    String.fromInt attr  ++ "px"
+    String.fromInt attr ++ "px"
+
+
 
 ---- OTHER ----
 
+
 type alias Size =
-    { width: Int
-    , height: Int
+    { width : Int
+    , height : Int
     }
 
+
+
 -- Search a term and get a url to be used in a query
-getUnsplashQuery : Model -> String
-getUnsplashQuery model =
-    let 
-        width = String.fromInt model.controls.width
-        height = String.fromInt model.controls.height
-        search = model.controls.query
+
+
+buildUnsplashQuery : Model -> String
+buildUnsplashQuery model =
+    let
+        width =
+            String.fromInt model.width
+
+        height =
+            String.fromInt model.height
+
+        search =
+            model.query
     in
-        "https://source.unsplash.com/" ++ width ++ "x" ++ height ++ "/?" ++ search
+    "https://source.unsplash.com/" ++ width ++ "x" ++ height ++ "/?" ++ search
+
+
 
 -- Return an image from a query (does not get a specific image via ID)
-getRandomImage : Model -> String -> Http.Request String
-getRandomImage model query =
-    Http.getString (getUnsplashQuery model)
+
+
+getImage : Model -> Cmd Msg
+getImage model =
+    Http.send UpdateImage (Http.getString (buildUnsplashQuery model))
